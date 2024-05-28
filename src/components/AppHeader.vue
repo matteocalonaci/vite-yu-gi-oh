6827304a5352b38d35ee1f3361fcce458b5f1a0e
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-28 12:10:19 +0200

    Modifica Dropdown non funzionante in select

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <!-- JS -->
 <script>
+import store from '../data/store.js';
+
 export default {
     name: "AppHeader",
     data() {
@@ -16,11 +18,25 @@ export default {
 <!-- HTML -->
 <template>
     <header>
-        <h1>Yu-Gi-Oh Api</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-1  p-2">
+                    <img src="../assets/Yugioh_anime_logo.webp" alt="logo">
+                </div>
+                <div class="col-1"></div>
+                <div class="col-10 text-end p-2">
+                    <h1>Yu-Gi-Oh Api</h1>
+                </div>
+            </div>
+        </div>
     </header>
 
 </template>
 
 
 <!-- CSS -->
-<style scoped></style>
+<style scoped>
+img {
+    width: 10rem;
+}
+</style>
